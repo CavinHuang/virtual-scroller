2998391825e5460cc6deb51ee001b5ad34a5e3d2
CavinHuang <sujinw@qq.com>
2023-03-03 10:16:26 +0800

    feat: 完成demo的loading

--- playground/vue3-demo/src/App.vue
+++ playground/vue3-demo/src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import { PullRefresh } from '@virsual-scroller/vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import List from './components/list.vue'
 const count = ref(0);
 const loading = ref(false);
 const onRefresh = () => {
@@ -15,6 +14,7 @@ const onRefresh = () => {
 </script>
 
 <template>
+  <List />
   <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -23,23 +23,12 @@ const onRefresh = () => {
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div> -->
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" />
   <PullRefresh v-model="loading" @refresh="onRefresh">
     <p>刷新次数: {{ count }}</p>
-  </PullRefresh>
+  </PullRefresh> -->
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
---
+++ playground/vue3-demo/src/components/list.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+    <PullRefresh v-model="loading" @refresh="onRefresh" :success-duration="1500">
+      <p>刷新次数: {{ count }}</p>
+      <template #loading>
+        <Loading />
+      </template>
+    </PullRefresh>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PullRefresh } from '@virsual-scroller/vue'
+import { ref } from 'vue';
+import Loading from './loading.vue'
+const count = ref(0);
+const loading = ref(false);
+const onRefresh = () => {
+  setTimeout(() => {
+    loading.value = false;
+    count.value++;
+  }, 10000);
+};
+</script>
+
+<style lang="scss" scoped>
+.list {
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+}
+</style>
